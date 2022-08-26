3e1e106805d185241c7212e9d58ccccb9761053b
Bamboo <michael.cawayan@gmail.com>
2022-08-26 18:56:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # normalize the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
